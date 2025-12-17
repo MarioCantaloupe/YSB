cf3227f0c4f8a3a2b9977a8c5403ffd4226000ec
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 12:14:56 +0100

    added item state resource script (currently unused)

---
+++ 02_scenes/02_objects/item_state.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ItemState
+
+@export var id: String
+@export var cook_level: int = 0
+@export var chop_level: int = 0
+@export var ice_level: int = 0
+@export var is_clean: bool = false
+@export var is_frozen: bool = false
