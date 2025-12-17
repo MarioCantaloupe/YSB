8d194fe5b9873ab14a2befd15f863e7830ad8527
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 12:16:03 +0100

    removed debug visuals, fixed bugs
    fixed some bugs where items would unstick from stations when clicking anywhere or another item was dropped on station

--- 02_scenes/02_objects/drop_zone.gd
+++ 02_scenes/02_objects/drop_zone.gd
@@ -1,20 +1,29 @@
 extends Node2D
-signal zone_selected
-var is_occupied : bool
+signal zone_selected(bool)
+var is_occupied : bool = false
 var held_item = null
 
-func _draw():
-	draw_circle(Vector2(0,0), 75, Color.BLANCHED_ALMOND) #TODO delete debug visuals
+#func _draw():
+	#draw_circle(Vector2(0,0), 75, Color.BLANCHED_ALMOND) #TODO delete debug visuals
 	
 func select(new_held_item):
+	# If another item was on this zone, clean it up fully
 	if held_item != null and held_item != new_held_item:
-		deselect()
+		# tell the previous item it's no longer on this station
+		held_item.clear_from_station()
+		# fully release our reference
+		held_item = null
+		is_occupied = false
+
 	held_item = new_held_item
-	modulate = Color.WEB_MAROON #TODO delete debug visuals
+	#modulate = Color.WEB_MAROON #TODO delete debug visuals
 	is_occupied = true
-	emit_signal("zone_selected")
+	emit_signal("zone_selected", true)
 
 func deselect():
 	modulate = Color.WHITE #TODO delete debug visuals
 	is_occupied = false
-	held_item.clear_from_station()
+	emit_signal("zone_selected", false)
+	if held_item != null:
+		held_item.clear_from_station()
+		held_item = null
