9fe6f644db7a2841de8a53c32b9eb98b44b39552
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 12:21:03 +0100

    implemented tap sprite vfx

--- 02_scenes/02_objects/station.gd
+++ 02_scenes/02_objects/station.gd
@@ -8,3 +8,16 @@ enum StationAction { NONE, COOK, CLEAN, DEFROST }
 func _ready() -> void:
 	if action == StationAction.NONE:
 		pass
+
+
+func _on_drop_zone_zone_selected(state):
+	if state == true:
+		if action == StationAction.DEFROST:
+			get_parent().enable_fire(true)
+		elif action == StationAction.CLEAN:
+			get_parent().enable_water(true)
+	else:
+		if action == StationAction.DEFROST:
+			get_parent().enable_fire(false)
+		elif action == StationAction.CLEAN:
+			get_parent().enable_water(false)
