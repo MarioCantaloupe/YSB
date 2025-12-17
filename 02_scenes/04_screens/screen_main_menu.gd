8eb0ca1842a45529f4094c05aa12b88f3315fe78
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 12:23:54 +0100

    naming change

---
+++ 02_scenes/04_screens/screen_main_menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_button_options_pressed() -> void:
+	var options = preload("res://02_scenes/04_screens/screen_options.tscn").instantiate()
+	get_tree().root.add_child(options)
+	
+	options.exited.connect(func():
+		options.queue_free()
+)
