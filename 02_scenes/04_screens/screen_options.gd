0728064f76a9ae4d4fab60bab32496a847ed2067
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 13:02:09 +0100

    added options screen popup animation

--- 02_scenes/04_screens/screen_options.gd
+++ 02_scenes/04_screens/screen_options.gd
@@ -1,7 +1,12 @@
 extends Control
 
 signal exited
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+func _ready() -> void:
+	animation_player.play("options_popup")
 
 func _on_button_exit_pressed() -> void:
+	animation_player.play_backwards("options_popup")
+	await animation_player.animation_finished
 	exited.emit()
