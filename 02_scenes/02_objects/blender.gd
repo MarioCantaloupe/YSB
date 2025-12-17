f49b85bbf519fa3b31de1eb92c59bd36428f387c
MarioCantaloupe <mariorezest@gmail.com>
2025-12-17 12:20:10 +0100

    removed debug prints

--- 02_scenes/02_objects/blender.gd
+++ 02_scenes/02_objects/blender.gd
@@ -48,7 +48,5 @@ func get_array_average_color() -> Color:
 
 func _on_drop_zone_mouse_entered() -> void:
 	can_drop = true
-	print(can_drop)
 func _on_drop_zone_mouse_exited() -> void:
 	can_drop = false
-	print(can_drop)
