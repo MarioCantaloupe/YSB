d76fb693f62c4947636b884a91897f50a89fa98d
MarioCantaloupe <mariorezest@gmail.com>
2025-12-21 02:23:01 +0100

    pause menu changes

---
+++ 02_scenes/03_levels/level.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@export var can_pause : bool
+@onready var pause_canvas_layer: CanvasLayer = $pause_canvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if can_pause:
+		pause_canvas_layer.visible = true
