b6f8a55dc379abd801e1243b4f4169b0ffbbce40
MarioCantaloupe <mariorezest@gmail.com>
2025-12-24 13:58:56 +0100

    added item heights

--- 02_scenes/02_objects/item/item_data.gd
+++ 02_scenes/02_objects/item/item_data.gd
@@ -6,6 +6,9 @@ class_name ItemData
 @export var average_color: Color
 @export var type : IngredientType
 @export var cooking_time : float = 3.0
+@export var ingredient_height_ch00 : int #in pixels, for positioning other ingredients
+@export var ingredient_height_ch01 : int
+@export var ingredient_height_ch02 : int
 
 @export var cookable : bool
 @export var cleanable : bool
