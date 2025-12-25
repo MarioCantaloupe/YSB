03b4cc981e764b26b52767d24458857e492c4b9f
MarioCantaloupe <mariorezest@gmail.com>
2025-12-25 11:46:13 +0100

    changed sprites, added is_holding setter

--- 02_scenes/01_characters/player_cursor.gd
+++ 02_scenes/01_characters/player_cursor.gd
@@ -9,27 +9,35 @@ var previous_mouse_pos
 
 var enabled = true
 
+# ICONS
+var icon_arrow = preload("res://01_assets/01_sprites/01_characters/cursor_default.png")
+
+
 var is_knife = false
-var is_holding = false
+var is_holding = false : set = _set_is_holding
 var held_item: RigidBody2D = null
+
+
 
 enum CursorType {
 	POINT,
 	CAN_INTERACT,
+	CAN_HOLD,
 	HOLDING,
 	KNIFE,
 }
 
 var cursor_images := { #TODO placeholders
-	CursorType.POINT: "res://01_assets/01_sprites/01_characters/cursor_default.png",
+	CursorType.POINT: "res://01_assets/01_sprites/01_characters/cursor_point.png",
 	CursorType.CAN_INTERACT: "res://01_assets/01_sprites/01_characters/hand_point.png",
-	CursorType.HOLDING: "res://01_assets/01_sprites/01_characters/cursor-png-1137.png",
+	CursorType.CAN_HOLD: "res://01_assets/01_sprites/01_characters/cursor_canhold.png",
+	CursorType.HOLDING: "res://01_assets/01_sprites/01_characters/cursor_holding.png",
 	CursorType.KNIFE: "res://01_assets/01_sprites/01_characters/cursor_knife.png",
 }
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	set_cursor(CursorType.POINT)
+	set_cursor(CursorType.POINT, Vector2(42,24))
 	Input.set_custom_mouse_cursor(load(cursor_images[1]), Input.CURSOR_POINTING_HAND)
 	previous_mouse_pos = get_viewport().get_mouse_position()
 
@@ -62,15 +70,16 @@ func toggle_knife():
 		set_cursor(CursorType.KNIFE)
 		emit_signal("knife_mode_changed", true)
 	else:
-		set_cursor(CursorType.POINT)
+		set_cursor(CursorType.POINT, Vector2(42,24))
 		emit_signal("knife_mode_changed", false)
 	
 	print("is_knife set to " + str(is_knife))
 	
-func set_cursor(cursor_type: CursorType):
+func set_cursor(cursor_type: CursorType, hotspot: Vector2 = Vector2.ZERO) -> void:
 	if cursor_images.has(cursor_type):
-		Input.set_custom_mouse_cursor(load(cursor_images[cursor_type]))
-		print("cursor changed to: " + str(cursor_type))
+		var texture: Texture2D = load(cursor_images[cursor_type])
+		Input.set_custom_mouse_cursor(texture, Input.CURSOR_ARROW, hotspot)
+		print("cursor changed to: %s (hotspot: %s)" % [cursor_type, hotspot])
 
 func enable_cursor(boolean : bool):
 	if boolean:
@@ -79,3 +88,6 @@ func enable_cursor(boolean : bool):
 	else:
 		PlayerCursor.enabled = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _set_is_holding(value):
+	is_holding = value
