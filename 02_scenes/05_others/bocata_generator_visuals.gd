6a0dd4f4c448b7d6d73eec8e8035a34c14998a32
MarioCantaloupe <mariorezest@gmail.com>
2025-12-25 11:48:14 +0100

    scrapped bocata gen visuals
    it's too goddamn hard man leave me alone

--- 02_scenes/05_others/bocata_generator_visuals.gd
+++ 02_scenes/05_others/bocata_generator_visuals.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
 @onready var bocataGen_logic: Node = $bocataGenerator_logic
-@export var item_scene : PackedScene
+var item_scene : PackedScene = preload("res://02_scenes/02_objects/item/item.tscn")
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("debug_key"):
-		display_bocata()
-
-func display_bocata():
+func display_bocata(): #should be called from outside
 	for item in get_tree().get_nodes_in_group("grabbables"):
 		item.queue_free()
 	
-	var item_pos : Vector2 = Vector2(640, 600)
-	bocataGen_logic.build_bocata()
+	var item_pos : Vector2 = global_position
+	var _bocata_height : int = 0 #measured in pixels
+	
 	for ingredient_state in bocataGen_logic.bocata_ingredients:
 		var new_ingredient = item_scene.instantiate()
-		get_tree().current_scene.add_child(new_ingredient)
-		#get_tree().current_scene.get_node("WorldItems").add_ch]ild(new_ingredient) #this code should hapen if the scene is correct
+		
+		if get_tree().current_scene.get_node("WorldItems"):
+			get_tree().current_scene.get_node("WorldItems").add_child.call_deferred(new_ingredient) #this code should hapen if the scene is correct
+		else:
+			get_tree().current_scene.add_child.call_deferred(new_ingredient)
+		await new_ingredient.ready
 		new_ingredient.set_gravity(false)
 		new_ingredient.shadow.visible = false
 		new_ingredient.collision.disabled = true
 		new_ingredient.apply_item_state(ingredient_state)
 		new_ingredient.position = item_pos
-		item_pos += Vector2(0, -50)
-		
+		item_pos += Vector2(0, new_ingredient.ingredient_height)
+		print("bocata add: " + str(_bocata_height) + "+" + str(new_ingredient.ingredient_height))
+		_bocata_height += new_ingredient.ingredient_height
+	print("final bocata height: " + str(_bocata_height))
