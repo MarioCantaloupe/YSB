f849c5c0b70dcc7f2163d70e87bb5c06b6e2fb54
MarioCantaloupe <mariorezest@gmail.com>
2025-12-25 11:46:31 +0100

    started order system

--- 02_scenes/00_autoload/game_system.gd
+++ 02_scenes/00_autoload/game_system.gd
@@ -1,12 +1,34 @@
 extends Node
 
-var rating : int
-var max_rating : int = 5
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var spaceship_integrity
+var drinks_unlocked : bool = false
+# Array to hold all currently active orders
+var active_orders: Array[OrderData] = []
+
+# Called when a new note is clicked
+func register_order(id: int, bocata_data: Array[ItemState], drink_data : Array[ItemState]):
+	var new_order = OrderData.new()
+	new_order.order_id = id
+	new_order.sandwich_ingredients = bocata_data
+	new_order.creation_time = Time.get_ticks_msec()
+	
+	new_order.drink_ingredients = drink_data
+	
+	print("GameSystem: Registered Order #", id, " with ", bocata_data.size(), " ingredients.")
+	
+	active_orders.append(new_order)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func complete_order(order_id: int):
+	for order in active_orders:
+		if order.order_id == order_id:
+			print("GameSystem: Order #", order_id, " completed!")
+			active_orders.erase(order)
+			return
+
+
+func get_order_by_id(order_id: int) -> OrderData:
+	for order in active_orders:
+		if order.order_id == order_id:
+			return order
+	return null
