c069122f1e622c20464ff3614669755405a3424f
MarioCantaloupe <mariorezest@gmail.com>
2025-12-28 01:46:47 +0100

    added some final runner sprites

--- 02_scenes/03_levels/infinite_runner.gd
+++ 02_scenes/03_levels/infinite_runner.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player: Player = $yaya
 @onready var lung_bar: ProgressBar = $UI/VBoxContainer/lung_bar
+@onready var texture_lung_bar: TextureProgressBar = $UI/lung_bar_progress/texture_lung_bar
 @onready var tooltip_player: AnimationPlayer = $tooltip_player
 
 var has_tooltip_appeared : bool = false
@@ -11,11 +12,14 @@ func _ready() -> void:
 	PlayerCursor.enable_cursor(false)
 	tooltip_player.play("control_tooltip_fadeIn")
 	
-	lung_bar.max_value = player.lung_capacity
-	lung_bar.value = player.lung_capacity
+	texture_lung_bar.max_value = player.lung_capacity
+	texture_lung_bar.value = player.lung_capacity
+	#lung_bar.max_value = player.lung_capacity
+	#lung_bar.value = player.lung_capacity
 
 func _process(_delta: float) -> void:
-	lung_bar.value = player.lung_capacity
+	#lung_bar.value = player.lung_capacity
+	texture_lung_bar.value = player.lung_capacity
 	if Input.is_action_just_pressed("jump") and not has_tooltip_appeared:
 		tooltip_player.play_backwards("control_tooltip_fadeIn")
 		has_tooltip_appeared = true
