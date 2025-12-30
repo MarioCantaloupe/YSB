827b7d8da51865d549da0985c1d69f242b38e776
MarioCantaloupe <mariorezest@gmail.com>
2025-12-30 22:43:34 +0100

    added obstacle data scale variance

--- 02_scenes/02_objects/01_obstacle_data/obstacle_data_resource.gd
+++ 02_scenes/02_objects/01_obstacle_data/obstacle_data_resource.gd
@@ -7,7 +7,8 @@ class_name ObstacleData
 @export var min_loot : int
 @export var max_loot : int
 @export var hit_sound : AudioStream
+@export var size_variance : float = 0
 
 var loot_amount : int = randi_range(min_loot, max_loot)
-
+	
 @export var sprite : Texture2D
