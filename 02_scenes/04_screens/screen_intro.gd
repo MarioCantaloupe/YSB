73e83b05dc5d1c8bd5d08a4aa914f182b6bc6514
MarioCantaloupe <mariorezest@gmail.com>
2025-12-30 22:42:23 +0100

    SceneLoader refactor

--- 02_scenes/04_screens/screen_intro.gd
+++ 02_scenes/04_screens/screen_intro.gd
@@ -6,9 +6,10 @@ var NextScene = "res://02_scenes/04_screens/screen_mainMenu.tscn"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animation_player.play("logo_fade_in")
-	await GlobalScript.wait(1.5)
-	GlobalScript.change_scene(NextScene)
+	await animation_player.animation_finished
+	await get_tree().create_timer(1.5).timeout
+	SceneLoader.load_scene(NextScene, SceneLoader.Transition.FADE)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		GlobalScript.change_scene(NextScene)
+		SceneLoader.load_scene(NextScene, SceneLoader.Transition.FADE)
