0c77a865d8ba9c988eb1d0596d3be373c02066fd
MarioCantaloupe <mariorezest@gmail.com>
2026-01-13 20:10:13 +0100

    updated order matching logic

--- 02_scenes/05_others/order_matcher.gd
+++ 02_scenes/05_others/order_matcher.gd
@@ -6,9 +6,14 @@ class_name OrderMatcher
 #TODO TODO TODO TODO TODO TODO MAKE IT MORE THOUROUGH LATER IF TIME CONSTRAINTS ALLOW
 
 static func match_order_simple(given : OrderData, expected: OrderData) -> int:
-	if given.bocata_ingredients == expected.bocata_ingredients \
-	and given.drink_ingredients == expected.drink_ingredients:
-		return 100
-	else:
-		return 0
 	
+	if given.bocata_ingredients == expected.bocata_ingredients and given.drink_ingredients == expected.drink_ingredients:
+		return 100
+	
+	if given.bocata_ingredients == expected.bocata_ingredients:
+		return 50
+		
+	if given.drink_ingredients == expected.drink_ingredients:
+		return 50
+	
+	return 0
