aa17af6ed6fdfa29037866c152091174538f3ee1
MarioCantaloupe <mariorezest@gmail.com>
2026-01-13 18:32:21 +0100

    updated blender interactions

--- 02_scenes/02_objects/stations/blender.gd
+++ 02_scenes/02_objects/stations/blender.gd
@@ -1,44 +1,50 @@
 extends Node2D
+class_name Blender
 
 @export var blender_audio : AudioStream
-@export var held_items : Array = []
+@export var states_in_cup : Array[ItemState] = []
 @onready var drop_zone: Area2D = $drop_zone
 @onready var button: Button = $Button
 @onready var blender_fluid: Sprite2D = $blender_fluid
 
-var can_drop : bool
 var blended_color : Color
 
 func _ready() -> void:
-	blended_color = Color(0.879, 0.824, 0.707, 0.255)
+	blended_color = GameSystem.blender_fluid_color
+	blender_fluid.self_modulate = blended_color
+	states_in_cup = GameSystem.ingredient_states_in_blender
 
 func _on_button_pressed() -> void:
 	blend()
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_released("Lclick"):
-		var input_item = PlayerCursor.held_item
-		print("dropped item: " + str(PlayerCursor.held_item))
-		if PlayerCursor.held_item and can_drop:
-			held_items.append(input_item.food_data.average_color)
-			blended_color = get_array_average_color()
-			PlayerCursor.held_item.queue_free()
+func _on_drop_zone_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if PlayerCursor.held_item == null:
+		return
+	
+	if event.is_action_released("Lclick"):
+		var input_state : ItemState = PlayerCursor.held_item.save_item_state()
+		print("dropped item: ", input_state.data.name)
+		states_in_cup.append(input_state)
+		GameSystem.ingredient_states_in_blender.append(input_state)
+		blended_color = get_array_average_color()
+		GameSystem.blender_fluid_color = get_array_average_color()
+		PlayerCursor.held_item.queue_free()
 
 func get_array_average_color() -> Color:
 	var sum_r := 0.0
 	var sum_g := 0.0
 	var sum_b := 0.0
 
-	if held_items.is_empty():
+	if states_in_cup.is_empty():
 		return Color(0.888, 0.866, 0.79, 0.388)  #cum
 
-	for item in held_items:
-		var c: Color = item
+	for state in states_in_cup:
+		var c: Color = state.data.average_color
 		sum_r += c.r
 		sum_g += c.g
 		sum_b += c.b
 
-	var count := float(held_items.size())
+	var count := float(states_in_cup.size())
 	return Color(
 		sum_r / count,
 		sum_g / count,
@@ -52,17 +58,11 @@ func blend():
 	
 	AudioManager.play_oneshot(blender_audio, 0, 1, 0, AudioManager.Bus.SFX)
 
-func _on_drop_zone_mouse_entered() -> void:
-	can_drop = true
-	print(can_drop)
-func _on_drop_zone_mouse_exited() -> void:
-	can_drop = false
-	print(can_drop)
-
-
 func _on_button_mouse_entered() -> void:
 	PlayerCursor.set_cursor(PlayerCursor.CursorType.CAN_INTERACT)
 
-
 func _on_button_mouse_exited() -> void:
 	PlayerCursor.set_cursor(PlayerCursor.CursorType.POINT)
+
+func finish_drink() -> Array[ItemState]:
+	return states_in_cup
