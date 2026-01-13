3ed2d2ad09f533f61d1f06772192ab2457df07ec
MarioCantaloupe <mariorezest@gmail.com>
2026-01-13 18:09:08 +0100

    updated to use ButtonMaster inheritance

--- 02_scenes/04_screens/button_change_scene.gd
+++ 02_scenes/04_screens/button_change_scene.gd
@@ -1,10 +1,15 @@
 extends DynamicButton
+class_name SceneChanger
 
 @export var target_scene_path : String
 @export var transition : SceneLoader.Transition = SceneLoader.Transition.FADE
 @export var use_loading_screen : bool = true
 
-func _on_pressed() -> void:
+func _ready() -> void:
+	super._ready()
+	pressed.connect(_change_scene)
+
+func _change_scene() -> void:
 	if PlayerCursor.is_knife or PlayerCursor.is_holding:
 		return
 	
